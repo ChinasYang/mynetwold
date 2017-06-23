b2aa1c388cef0a1a3048a8ec2a78235e50e1c39d
chaoshen <woqunimade@xiao.com>
2017-06-23 11:41:31 +0800

    first

---
+++ mynetworld/js/index.js
@@ -0,0 +1,35 @@
+// $(document).ready(function() { 
+//  	// $(".top1").animate(
+//  	// 	{width:"900px"},
+//  	// 	{height:"800px"},
+//  	// 	{translateZ:"(-720deg)"},
+//  	// 	{perspective:"720"},
+//  	// 	{transform:"rotate(7deg)";},
+//  	// 	{width:"196px"},
+//  	// 	{height:"60"}
+//  	// 	,4000);
+//  function aa(){
+//   			var moveStep=4;
+// 	        var oSecond=document.getElementById("second");
+// 	        var oLunbo=oSecond.getElementsByClassName("second-lunbo");
+// 	        var length=oLunbo.length;
+
+// 	        var timer=setInterval(
+// 	          function(){
+// 	            for(var i=0;i<oLunbo.length;i++){   
+// 	              if( parseInt( oLunbo[i].style.left)<=-1*parseInt(getStyle(oSecond,"width"))  )
+// 	              {
+// 	                oLunbo[i].style.left=(length-1)*parseInt(getStyle(oSecond,"width"))+"px";
+// 	              }
+// 	              oLunbo[i].style.left=parseInt(getStyle(oLunbo[i],"left"))-moveStep+"px";
+// 	            }
+// 	          },1);
+
+// 	        function getStyle(obj,attr){
+// 	          return obj.currentStyle? obj.currentStyle[attr]:getComputedStyle(obj,null)[attr];
+// 	        }
+
+  		
+// 		}
+
+// })
---
+++ mynetworld/top.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="swiper-slide">
+		<div class='top1'>WORKS</div>
+	</div>
+</template>
+<style>
+	.top1{
+		width:150px;
+		height:60px;
+		border-top:3px solid #fff;
+		border-bottom:3px solid #fff;
+		font-size:30px;
+		line-height:60px;
+		color:#fff;
+	}
+</style>
